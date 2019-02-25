6f60985ede5efd1c502900c84ee8a92e9e8549c5
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2019-02-26 02:51:11 +0530

    Connect Activity to the ViewModel. TextView's value now survives the configuration change. At this stage, the app is the simplest example of the Android Architechture Components (ViewModel, Observer, LiveData).

--- app/src/main/java/basicexample/androidarchitecturecomponentssimpleexample/MainActivity.kt
+++ app/src/main/java/basicexample/androidarchitecturecomponentssimpleexample/MainActivity.kt
@@ -1,5 +1,7 @@
 package basicexample.androidarchitecturecomponentssimpleexample
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -7,20 +9,45 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // Provide ViewModel to the Activity
+    private val viewModel: MyViewModel by lazy {
+        ViewModelProviders.of(this).get(MyViewModel::class.java)
+    }
+
+    // Define what to with the value once we are notified of the change.
+    // We are updating the text of TextView with the provided Int value.
+    // TODO: Use SAM constructor
+    // What this Observer does (its behaviour) is provided through its constructor function.
+    private val changeObserver = Observer<Int>(fun(intValue: Int?) {
+        if (intValue != null) {
+            updateTextView(intValue)
+        }
+    })
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        my_container.setOnClickListener { incrementCount() }
+
+        // Register/subscribe the Observer with the ViewModel
+        // observe() documentation has useful info
+        viewModel.changeNotifier.observe(this, changeObserver)
+
+        // User changes the data through Activity
+        // Activity updates the data through ViewModel object reference
+        // Activity can also send any data to ViewModel through this function
+        my_container.setOnClickListener { incrementValue() }
     }
 
     override fun onResume() {
         super.onResume()
-        incrementCount()
+        incrementValue()
     }
 
-    private var count: Int = 0
+    private fun incrementValue() {
+        viewModel.increment()
+    }
 
-    private fun incrementCount() {
-        my_text.text = (++count).toString()
+    private fun updateTextView(intValue: Int) {
+        my_text.text = intValue.toString()
     }
 }
